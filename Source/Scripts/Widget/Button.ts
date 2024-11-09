11f830d6bf0728d4d1e394f10586468a30b0bf7c
JJCoder <JJCoder@openzilab.com>
2024-11-09 10:40:45 +0800

    feat(Button): 为 Button 组件添加新功能和属性
    -增加 isFocusable 属性，用于控制按钮是否可聚焦- 添加 content 属性，支持自定义按钮内容
    - 实现 setContent 方法，用于设置按钮内容
    - 引入 MultiEvent 类，支持事件的多次触发
    - 更新 onClick 属性为 MultiEvent 类型，增强事件处理能力
    - 添加 hAlign 和 vAlign 属性，用于控制内容的水平和垂直对齐
    - 更新 Button 类的构造函数，支持传入 WidgetProps 参数

--- Source/Scripts/Widget/Button.ts
+++ Source/Scripts/Widget/Button.ts
@@ -1,15 +1,28 @@
 import { Widget } from "./Widget";
+import { EHAlign, EVAlign, TAttribute, WidgetProps } from "./IType";
+import { MultiEvent } from "../Core/MultiEvent";
 
 export class Button extends Widget {
-    text: string;
-    onClick: () => void;
+    text: TAttribute<string>
+    isFocusable: boolean
+    content: Widget;
+    hAlign: EHAlign
+    vAlign: EVAlign
 
-    constructor() {
+    onClick: MultiEvent<() => void>;
+
+    // content: Widget | () => Widget;
+
+    constructor(props: WidgetProps<Button>) {
         super();
     }
 
     setText(text: string) {
         this.text = text;
+    }
+
+    setContent(content: Widget) {
+        this.content = content;
     }
 
 }
